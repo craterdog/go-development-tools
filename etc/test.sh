d9434dd0bcebb33a3a249faf7ac0d1d3dd12de3a
Derk Norton <derk.norton@gmail.com>
2024-12-17 16:48:17 -0700

    Regenerated the tests.

--- etc/test.sh
+++ etc/test.sh
@@ -1,3 +1,23 @@
+echo "Removing the old binaries..."
+rm -rf ./bin
+mkdir -p ./bin/
+echo "Done."
+echo
+
+echo "Compiling the following tools:"
+echo "	format-model"
+go build -o ./bin/ ./src/format-model
+echo "	format-syntax"
+go build -o ./bin/ ./src/format-syntax
+echo "	generate-classes"
+go build -o ./bin/ ./src/generate-classes
+echo "	generate-module"
+go build -o ./bin/ ./src/generate-module
+echo "	generate-packages"
+go build -o ./bin/ ./src/generate-packages
+echo "Done."
+echo
+
 directory=./tst/go-syntax-notation
 moduleName=github.com/craterdog/${directory:6}/v5
 wikiPath=https://github.com/craterdog/${directory:6}/wiki
@@ -38,11 +58,13 @@ directory=./tst/go-collection-framework
 moduleName=github.com/craterdog/${directory:6}/v5
 wikiPath=https://github.com/craterdog/${directory:6}/wiki
 bin/format-model ${directory}/v5/agent/Package.go
+echo
 bin/format-model ${directory}/v5/collection/Package.go
 echo
 bin/generate-module ${moduleName} ${wikiPath} ${directory}/v5/ agent collection
 echo
 bin/generate-classes ${moduleName} ${directory}/v5/ agent
+echo
 bin/generate-classes ${moduleName} ${directory}/v5/ collection
 echo
 cd ${directory}/v5
--- tst/go-class-model/v5/grammar/scanner.go
+++ tst/go-class-model/v5/grammar/scanner.go
@@ -190,9 +190,9 @@ loop:
 	for v.next_ < uint(len(v.runes_)) {
 		switch {
 		// Find the next token type.
+		case v.foundToken(DelimiterToken):
 		case v.foundToken(NewlineToken):
 		case v.foundToken(SpaceToken):
-		case v.foundToken(DelimiterToken):
 		case v.foundToken(CommentToken):
 		case v.foundToken(PrefixToken):
 		case v.foundToken(NameToken):
@@ -276,9 +276,9 @@ const (
 	upper_   = "\\p{Lu}"
 
 	// Define the regular expression patterns for each token type.
+	delimiter_    = "(?:type|package|map|iota|interface|import|func|const|chan|\\}|\\{|\\]|\\[|\\)|\\(|=|// TYPE DECLARATIONS|// Principal Methods|// INSTANCE DECLARATIONS|// Function Methods|// FUNCTIONAL DECLARATIONS|// Constructor Methods|// Constant Methods|// CLASS DECLARATIONS|// Attribute Methods|// Aspect Interfaces|// ASPECT DECLARATIONS|,)"
 	newline_      = "(?:" + eol_ + ")"
 	space_        = "(?:[ \\t]+)"
-	delimiter_    = "(?:type|package|map|iota|interface|import|func|const|chan|\\}|\\{|\\]|\\[|\\)|\\(|=|// TYPE DECLARATIONS|// Principal Methods|// INSTANCE DECLARATIONS|// Function Methods|// FUNCTIONAL DECLARATIONS|// Constructor Methods|// Constant Methods|// CLASS DECLARATIONS|// Attribute Methods|// Aspect Interfaces|// ASPECT DECLARATIONS|,)"
 	alphanumeric_ = "(?:" + lower_ + "|" + upper_ + "|" + digit_ + ")"
 	character_    = "(?:" + lower_ + "|" + upper_ + ")"
 	comment_      = "(?:/\\*" + eol_ + "(" + any_ + "|" + eol_ + ")*?" + eol_ + "\\*/" + eol_ + ")"
